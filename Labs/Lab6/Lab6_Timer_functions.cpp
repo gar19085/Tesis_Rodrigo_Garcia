7a4ce8bf0f3b674ce3b43dad263a535eef7803fb
gar19085 <gar19085@uvg.edu.gt>
2023-08-14 21:34:55 -0600

    Labs

--- Labs/Lab6/Lab6_Timer_functions.cpp
+++ Labs/Lab6/Lab6_Timer_functions.cpp
@@ -7,3 +7,10 @@
 #include <fcntl.h>
 #include <sched.h>
 
+#define MI_PRIORIDAD 1
+
+    struct sched_param param;
+    param.sched_priority = MI_PRIORIDAD;
+
+    
+    sched_setscheduler(0, SCHED_FIFO, &param);
